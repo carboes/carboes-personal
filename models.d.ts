5c2b700136a82ee518b48fec34c9892deceab56f
Carl Peterken <carboes@point37.com>
2021-11-25 23:48:19 +1300

    deploy

--- models.d.ts
+++ models.d.ts
@@ -5,4 +5,4 @@ import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import { GLBType } from './types';
 export declare const setLoaded: (_loaded: boolean) => void;
 export declare const checkLoaded: (scene: Scene, gltf: GLTF) => void;
-export declare const loadModels: (scene: Scene, mixers: AnimationMixer[], renderer: WebGLRenderer, modelPath: string, glbtype: GLBType, compress?: boolean) => void;
+export declare const loadModels: (scene: Scene, mixers: AnimationMixer[], renderer: WebGLRenderer, modelPath: string, glbtype: GLBType, compression?: boolean, staticPath?: string) => void;
