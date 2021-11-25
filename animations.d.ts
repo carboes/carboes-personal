361d7058055fc2ab650988a10ce66ab80d7a2627
Carl Peterken <carboes@point37.com>
2021-11-25 20:01:56 +1300

    deploy

--- animations.d.ts
+++ animations.d.ts
@@ -1,3 +1,3 @@
-import { AnimationMixer, WebGLRenderer } from "three";
-import { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
-export declare const loadAnimations: (gltf: GLTF, mixers: AnimationMixer[], renderer: WebGLRenderer) => void;
+import { AnimationMixer, WebGLRenderer } from 'three';
+import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
+export declare const loadAnimations: (gltf: GLTF, mixers: AnimationMixer[], renderer: WebGLRenderer, autoplay?: boolean) => void;
