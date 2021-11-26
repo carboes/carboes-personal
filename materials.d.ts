addc89e8343a0d18ca922c1fd65dc4ed7388b13e
Carl Peterken <carboes@point37.com>
2021-11-26 13:59:56 +1300

    deploy

--- materials.d.ts
+++ materials.d.ts
@@ -1,4 +1,4 @@
 import { Color, Mesh, WebGLRenderer } from 'three';
 export declare const assignHolographicMaterial: (mesh: Mesh) => void;
 export declare const assignMetalMaterial: (mesh: Mesh, colorScalar?: number) => void;
-export declare const assignHDRMaterial: (mesh: Mesh, colorScalar: number | undefined, renderer: WebGLRenderer, color?: Color | undefined, staticPath?: string) => void;
+export declare const assignHDRMaterial: (mesh: Mesh, colorScalar: number | undefined, renderer: WebGLRenderer, color?: Color | undefined, staticPath?: string, opacity?: number) => void;
