23b39daed3f5acfe986f802f3e8ed7798728aedc
Carl Peterken <carboes@point37.com>
2021-12-02 18:53:55 +1300

    deploy

---
+++ new/src/types.d.ts
@@ -0,0 +1,23 @@
+import { RenderMessage } from 'common/src/character/types/character-render-message';
+import { Vector3 } from 'three';
+import { Color as CharacterColor, Genesis } from 'common/src/character/types';
+import { GLBType } from 'common/src/glb-iframe/types';
+export declare type GLBSetting = {
+    target: Vector3;
+    autoRotate: boolean;
+    cameraPosition: Vector3;
+};
+export declare type GLBSettings = {
+    [key in GLBType]: GLBSetting;
+};
+export declare type OpenSeaData = {
+    type: GLBType;
+    glb: string;
+    baseUri: string;
+    meta?: RenderMessage;
+};
+export declare type CharacterMeta = {
+    color: CharacterColor;
+    genesis: Genesis;
+    configurable: boolean;
+};
