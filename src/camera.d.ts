95acd17897b7b541ee4bda68cf1baea5f6331332
Carl Peterken <carboes@point37.com>
2021-12-05 01:59:50 +1300

    deploy

--- src/camera.d.ts
+++ src/camera.d.ts
@@ -1,2 +1,2 @@
 import { PerspectiveCamera, Vector3 } from 'three';
-export declare const loadCamera: (vector?: Vector3 | undefined, isBrain?: boolean | undefined) => PerspectiveCamera;
+export declare const loadCamera: (vector?: Vector3 | undefined, isBrain?: boolean | undefined, isHolographic?: boolean | undefined) => PerspectiveCamera;
